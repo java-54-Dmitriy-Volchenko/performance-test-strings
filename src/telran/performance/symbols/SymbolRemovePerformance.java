e50c44f456fdd5a402902065755e70ccc9a96413
DmitriyVolchenko <Dmitriy.Volchenko@gmail.com>
2024-03-23 09:30:36 +0200

    rewrite class SymbolRemovePerformance to change field symbol to implement receiving symbol by instance of class at the moment of its call.

--- src/telran/performance/symbols/SymbolRemovePerformance.java
+++ src/telran/performance/symbols/SymbolRemovePerformance.java
@@ -1,24 +1,30 @@
 package telran.performance.symbols;
 
+import java.util.Random;
 import telran.performance.PerformanceTest;
 import telran.symbols.RemoveSymbol;
 
 public class SymbolRemovePerformance extends PerformanceTest {
-	String str;
-	char symbol;
-	RemoveSymbol symbolRemove ;
-	public SymbolRemovePerformance(String displayName, int nRuns,
-			RemoveSymbol symbolRemove, String str, char symbol) {
-		super(displayName, nRuns);
-		this.symbolRemove = symbolRemove;
-		this.str = str;
-		this.symbol = symbol;
-			}
-	
-	@Override
-	protected void runTest() {
-		symbolRemove.removeSymbol(str, symbol);
+    String str;
+    char symbol;
+    RemoveSymbol symbolRemove;
 
-	}
+    public SymbolRemovePerformance(String displayName, int nRuns,
+            RemoveSymbol symbolRemove, String str) {
+        super(displayName, nRuns);
+        this.symbolRemove = symbolRemove;
+        this.str = str;
+        this.symbol = getRandomAsciiSymbol();
+    }
 
+    @Override
+    protected void runTest() {
+        symbolRemove.removeSymbol(str, symbol);
+    }
+//got method from chatGPT
+    private char getRandomAsciiSymbol() {
+        Random random = new Random();
+        int randomNumber = random.nextInt(126 - 32 + 1) + 32;
+        return (char) randomNumber;
+    }
 }
