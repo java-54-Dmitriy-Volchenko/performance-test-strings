b90f595243bb84b7889e982eca13d58e4f4db2ad
DmitriyVolchenko <Dmitriy.Volchenko@gmail.com>
2024-03-23 09:25:14 +0200

    rewrites methods SymbolRemoveCharArray and SymbolRemoveStandart

--- src/telran/symbols/SymbolsRemoveCharArray.java
+++ src/telran/symbols/SymbolsRemoveCharArray.java
@@ -1,5 +1,20 @@
 package telran.symbols;
 
-public class SymbolsRemoveCharArray {
+public class SymbolsRemoveCharArray implements RemoveSymbol {
+
+	@Override
+	public String removeSymbol(String str, char symbol) {
+		  char[] arrayFromString = str.toCharArray();
+		  char[] newArray = new char[arrayFromString.length];
+		  int index = 0;
+		  
+		  for (int i = 0; i<arrayFromString.length; i++) {
+			  if (arrayFromString[i] != symbol) {
+	                newArray[index] = arrayFromString[i];
+	                index++;
+	            }
+		  }
+		return new String(newArray, 0, index);
+	}
 
 }
--- src/telran/symbols/SymbolsRemoveStandard.java
+++ src/telran/symbols/SymbolsRemoveStandard.java
@@ -1,5 +1,12 @@
 package telran.symbols;
 
-public class SymbolsRemoveStandard {
+public class SymbolsRemoveStandard implements RemoveSymbol {
+
+	@Override
+	public String removeSymbol(String str, char symbol) {
+		
+		  return str.replaceAll(String.valueOf(symbol), "");
+		
+	}
 
 }
